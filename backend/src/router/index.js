62529a3aa09b570f3cda79db8c67216346349161
bigboy <moshazly416@gmail.com>
2024-10-19 16:36:48 +0300

    Create Login, Request-Password and Rest-Password Vue Components

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -2,23 +2,34 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import Login from '../views/Login.vue';
 import Dashboard from '../views/Dashboard.vue';
+import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
 
 const routes = [
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard
-  }
+    {
+        path: '/',
+        name: 'dashboard',
+        component: Dashboard
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login
+    },
+    {
+        path: '/request-password',
+        name: 'requestPassword',
+        component: RequestPassword
+    },{
+        path: '/reset-password',
+        name: 'resetPassword',
+        component: ResetPassword
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 });
 
 export default router;
